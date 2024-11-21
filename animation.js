06087da5a516f82ba9176d74befc7aa64904b093
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2024-11-21 09:33:45 +0100

    Updated algorithm to our project

--- animation.js
+++ animation.js
@@ -1,62 +1,82 @@
-window.addEventListener("load", init)
-
-const button = document.querySelector("#start-button")
+window.addEventListener("load", init);
 
 let gameInterval;
 
 let i = 0;
 let j = 0;
+let array = [5, 3, 7, 9, 4];
 
-let array = [5,3,7,9,4];
+function init() {
+  const button = document.querySelector("#start-button");
+  // const inputField = document.querySelector('.inputField');
+  // inputField.addEventListener('change', generateArray);
 
-function init () {
-    console.log("Programmet kører");
-    button.addEventListener("click", startSort)
+  button.addEventListener("click", startSort);
+  renderArray();
 }
 
-function startSort () {
-    console.log("Der klikkes på knappen");
-    gameInterval = setInterval(() => {
-        bubbleSort(array), 2000
-    })
-    //bubbleSort(array);
-    console.log(array);
+function startSort() {
+  gameInterval = setInterval(() => {
+    bubbleSort(array);
+    renderArray();
+  }, 2000);
 }
 
 function bubbleSort(array) {
-    if(isSorted(array)) {
-        clearInterval(gameInterval)
-        return;
-    }
-    if(i < array.length - 1) {
-        console.log("i, " + i)
-        compareValues();
-    }
-    console.log(array);
-    return array;
+  if (isSorted(array)) {
+    clearInterval(gameInterval);
+    return;
+  }
+  if (i < array.length - 1) {
+    compareValues();
+  }
+  return array;
 }
 
 function isSorted(array) {
   for (let i = 0; i < array.length - 1; i++) {
-    if (!(array[i] <= array[i + 1]))
-    return false;
+    if (!(array[i] <= array[i + 1])) return false;
   }
   return true;
 }
 
 function compareValues() {
-  console.log("j, " + j);
-  console.log("current " + array[j]);
-  console.log("next " + array[j + 1]);
   if (array[j] > array[j + 1]) {
     let temp = array[j];
     array[j] = array[j + 1];
     array[j + 1] = temp;
   }
-  if (j < array.length - i - 1) {
-    j++;
-  } else {
+  j++;
+  renderArray();
+
+  if (j >= array.length - 1 - i) {
     j = 0;
     i++;
   }
 }
+
+function showArray(element) {
+  const container = document.querySelector("#container");
+
+  const html = /*html*/ `
+  <div class="box" >${element}</div>
+  `;
+
+  container.insertAdjacentHTML("beforeend", html);
+  const boxes = document.querySelectorAll(".box");
+  if (boxes[j] && boxes[j + 1]) {
+    boxes[j].classList.add("lookingAt");
+    boxes[j + 1].classList.add("lookingAt");
+  }
+}
+
+function renderArray() {
+  const container = document.querySelector("#container");
+  container.innerHTML = "";
+  array.forEach(showArray);
+}
+
+// function generateArray(event) {
+//     event.preventDefault();
+//     console.log(event);
+// }
